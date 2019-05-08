19117ce639f36c4b19e2b0e4f01d4541148544df
KiwiBoaye <42769725+KiwiBoaye@users.noreply.github.com>
2019-05-08 15:08:59 +0200

    Add files via upload
    simple BinarySearch implementation

---
+++ BinSearch.java
@@ -0,0 +1,54 @@
+package zada4ka;
+
+public class BinSearch {
+
+    public static void main(String[] args) {
+
+        int[] array = {1, 2, 3, 4, 5, 6};
+
+
+        System.out.println(binSearch(array, 4));
+    }
+
+    public static int[] sortedArray(int[] array){
+
+        // Sorting MergeSort!!!
+
+        int[] newArr = new int[array.length];
+
+        int left = array[0];
+        int right = array.length - 1;
+        int pivot;
+
+        while (l) { // To be continued!!!
+
+
+        }
+
+        return newArr;
+    }
+
+    public static boolean binSearch(int[] array, int num) {
+
+        int l = array[0];
+        int r = array[array.length - 1];
+        int median;
+
+        while (r >= l) {
+            median = (l + r) / 2;
+
+            if (num == median) {
+                System.out.print("found: " + num + " = ");
+                return true;
+            } else {
+                if (num < median) {
+                    r = median - 1;
+                } else {
+                    l = median + 1;
+                }
+            }
+            System.out.println("iteration");
+        }
+        return false;
+    }
+}
